c76a1cb5e002947df5cc54fb4874e11e19851a43
businessFawn <BusinessFawn@users.noreply.github.com>
2022-08-14 20:03:11 -0400

    updated to allow for commands to invoke the suggest. Allowed the ability to add a new spot. Added the ability to record when we last ate at a place

--- dinner_bot/dinner_bot.py
+++ dinner_bot/dinner_bot.py
@@ -1,5 +1,7 @@
 import json
 import os
+import urllib.parse
+from random import randint
 
 import urllib3
 
@@ -9,31 +11,50 @@ from dinner_table import DinnerTable
 dinner_table = DinnerTable()
 http = urllib3.PoolManager()
 slack_url = os.getenv('WEBHOOK_URL')
+secret_path = os.getenv('SECRET_PATH')
 
 
 def lambda_handler(event, context):
+	secret = event['params']['path']['secret']
+	if secret != secret_path:
+		raise Exception("That isn't going to work!")
 	event_name = event['event_name']
 
 	if event_name == DinnerBotEventName.SUGGEST:
 		text_template = get_suggestions(event)
+		body = {
+			"text": text_template
+		}
+		encoded_data = json.dumps(body)
+		print(encoded_data)
+		r = http.request(
+			'POST', slack_url, body=encoded_data,
+			headers={'Content-type': 'application/json'}
+		)
+		print(r.status)
+		print(r.data)
+	elif event_name == DinnerBotEventName.RECORD:
+		prepared_name = extract_res_name(event)
+		try:
+			dinner_table.record(prepared_name)
+		except Exception as e:
+			return {'status': 'Failed', 'msg': e}
+
+	elif event_name == DinnerBotEventName.ADD:
+		dinner_table.add(extract_res_name(event), 1)
 	else:
 		raise NotImplementedError("No valid event name")
-	body = {
-		"text": text_template
-	}
-	encoded_data = json.dumps(body)
-	print(encoded_data)
-	r = http.request(
-		'POST', slack_url, body=encoded_data,
-		headers={'Content-type': 'application/json'}
-	)
-	print(r.status)
-	print(r.data)
-	return json.dumps({"status": "Success"})
+
+	return {"status": "Success"}
 
 
 def get_suggestions(event):
-	suggestions = [pretty_print(i['ResName']) for i in dinner_table.get_suggestions()]
+
+	formatted_suggestions = [pretty_print(i['ResName']) for i in dinner_table.get_suggestions()]
+	suggestions = []
+	for i in range(5):
+		my_int = randint(0, len(formatted_suggestions) - 1)
+		suggestions.append(formatted_suggestions.pop(my_int))
 
 	return 'It is time to pick food for tonight! We could have ' + ' or '.join(suggestions) + '!'
 
@@ -48,5 +69,29 @@ def pretty_print(res_name: str) -> str:
 	return rtn_str
 
 
+def extract_res_name(event):
+	body_params = event['body'].split('&')
+	print(body_params)
+	body = {i[0]: i[1] for i in [item.split('=') for item in body_params]}
+	res_name = body['text']
+	prepared_name = capitalize_name(res_name)
+	return prepared_name
+
+
+def capitalize_name(res_name: str) -> str:
+	return urllib.parse.unquote(res_name).title().replace(' ', '')
+
+
 if __name__ == '__main__':
-	lambda_handler({'event_name': DinnerBotEventName.SUGGEST}, {})
+	upvote_dict = {'params': {'path': {'secret': secret_path}, 'querystring': {}, 'header':
+		{'Accept': 'application/json,*/*', 'Accept-Encoding': 'gzip,deflate',
+		 'Content-Type': 'application/x-www-form-urlencoded', 'Host': 'l2pmhjm02g.execute-api.us-east-1.amazonaws.com',
+		 'User-Agent': 'Slackbot 1.0 (+https://api.slack.com/robots)',
+		 'X-Amzn-Trace-Id': 'Root=1-62f975d2-748e4756756ba4c57ffef1c9', 'X-Forwarded-For': '3.239.114.176',
+		 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https', 'X-Slack-Request-Timestamp': '1660515794',
+		 'X-Slack-Signature': 'v0=b22170b3062f34b87252d1459b9f3737dd1d1e3af543f1c182a0a7e3213e0e9e'}},
+				   'body': 'token=nPFTWyM3M22ntvMVAp2TdhEk&team_id=TL24XMDJP&team_domain=mixed-feelings&channel_id=C03TLFH28N7&channel_name=johns-channel&user_id=UL76E8UMP&user_name=konderla.john&command=%2Fdb_record&text=mellow mushroom&api_app_id=A03TP2E8R7W&is_enterprise_install=false&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTL24XMDJP%2F3932210732822%2Fia7FhbX7nx4QEVE62y1MZB7n&trigger_id=3938823935058.682167727635.1027454cb4e022bee844502b88eae2a2',
+				   'event_name': 'UPVOTE'}
+
+	print(lambda_handler(upvote_dict, {}))
+# lambda_handler({'event_name': DinnerBotEventName.SUGGEST}, {})
--- dinner_bot/dinner_table.py
+++ dinner_bot/dinner_table.py
@@ -1,12 +1,13 @@
 import os
-
+from datetime import datetime
 import boto3
-from boto3.dynamodb.types import TypeDeserializer
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 
 
 class DinnerTable:
 	def __init__(self):
 		self.deserializer = TypeDeserializer()
+		self.serializer = TypeSerializer()
 		if os.getenv('LOACL'):
 			self.session = boto3.Session(profile_name='default')
 		self.name = os.getenv('DINNER_TABLE_NAME')
@@ -16,6 +17,40 @@ class DinnerTable:
 	def get_suggestions(self):
 		res = self.dynamodb_client.scan(TableName=self.name)
 		return [self._deserialize(item) for item in res['Items']]
+
+	def record(self, res_name):
+		res_list = self.get_res(res_name)
+		if not res_list:
+			raise Exception("Res not found!")
+		res = res_list[0]
+		votes = res.get('Votes', 0)
+		votes += 1
+		res['Votes'] = votes
+		res['LastVote'] = int(datetime.utcnow().timestamp())
+		self.dynamodb_client.put_item(TableName=self.name, Item=self._serialize(res))
+
+	def add(self, res_name, dist):
+		res_list = self.get_res(res_name)
+		if res_list:
+			raise Exception("Found existing!")
+
+		self.dynamodb_client.put_item(TableName=self.name,
+									  Item=self._serialize({"ResName": res_name, "Distance": str(dist)}))
+
+	def get_res(self, res_name):
+		res = self.dynamodb_client.query(
+			TableName=self.name,
+			KeyConditionExpression='ResName = :val1',
+			ExpressionAttributeValues={':val1': {'S': res_name}}
+		)
+		return [self._deserialize(item) for item in res['Items']]
+
+
+	def _serialize(self, raw_data):
+		result = {}
+		for key, val in raw_data.items():
+			result[key] = self.serializer.serialize(val)
+		return result
 
 	def _deserialize(self, raw_data):
 		result = {}
